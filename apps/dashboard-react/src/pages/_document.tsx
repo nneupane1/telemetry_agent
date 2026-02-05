b307379b517faa043937719f38f6a4e135d8737d
Nischal Neupane <43709352+nneupane1@users.noreply.github.com>
2026-02-05 21:30:44 +0100

    Update _document.tsx

--- apps/dashboard-react/src/pages/_document.tsx
+++ apps/dashboard-react/src/pages/_document.tsx
@@ -1,1 +1,32 @@
 
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html lang="en" className="dark">
+      <Head>
+        {/* Primary theme color for browser UI */}
+        <meta name="theme-color" content="#05070D" />
+
+        {/* Performance & rendering hints */}
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="color-scheme" content="dark" />
+
+        {/* Favicon (placeholder, replace later) */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <body
+        className="
+          bg-bg-primary
+          text-text-primary
+          antialiased
+          overflow-x-hidden
+        "
+      >
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
